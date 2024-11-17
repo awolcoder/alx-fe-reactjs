b571eb5c3c97f8d713134cb019f4fd38f0b5f346
awolcoder <abdulbasitawil42@gmail.com>
2024-11-17 17:55:53 +0300

    Add recipeStore.js in store directory for Zustand state management

--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// Task 1 main.jsx updated
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -10,10 +14,6 @@ createRoot(document.getElementById('root')).render(
 )
 
 // Task 1 main.jsx updated
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
