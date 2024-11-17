97b1d74b8a3a43e21dc9ca4cbdf3687f41bb0bde
awolcoder <abdulbasitawil42@gmail.com>
2024-11-17 17:13:12 +0300

    Initial commit of Recipe Application

--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -8,3 +8,14 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+// Task 1 main.jsx updated
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
