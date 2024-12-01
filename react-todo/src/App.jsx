b579ec0dd45d719b203d5f597f995d8f88f4c904
awolcoder <abdulbasitawil42@gmail.com>
2024-12-01 23:20:04 +0300

    Implement Todo List component with add, toggle, delete functionality and add tests

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import TodoList from './TodoList';
 
-const App = () => {
-    return (
-        <div>
-            <TodoList />
-        </div>
-    );
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
 }
 
-export default App
+export default App;
