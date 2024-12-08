74bdb26a7c91d8b7217e1880bd001c3215c94848
awolcoder <abdulbasitawil42@gmail.com>
2024-12-08 17:54:18 +0300

    Initial setup with React and Tailwind CSS

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,16 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+module.exports = {
+  cobntent: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
