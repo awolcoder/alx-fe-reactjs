3b6e74006e4b9cff079a450539196590282098cb
awolcoder <abdulbasitawil42@gmail.com>
2024-12-18 12:09:55 +0300

    Enhanced Search Component

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,14 +1,30 @@
+// import axios from 'axios';
+
+// const BASE_URL = 'https://api.github.com/users';
+
+// const fetchUserData = async (username) => {
+//     try {
+//         const response = await axios.get(`${BASE_URL}/${username}`);
+//         return response.data;
+//     } catch (error) {
+//         throw new Error('Failed to fetch user data.');
+//     }
+// };
+
+// export default fetchUserData;
+
 import axios from 'axios';
 
-const BASE_URL = 'https://api.github.com/users';
+const BASE_URL = 'https://api.github.com/search/users';
 
-const fetchUserData = async (username) => {
+const fetchAdvancedUserData = async (query) => {
     try {
-        const response = await axios.get(`${BASE_URL}/${username}`);
+        const response = await axios.get(`${BASE_URL}?q=${query}`);
         return response.data;
     } catch (error) {
-        throw new Error('Failed to fetch user data.');
+        console.error('Error fetching user data:', error);
+        throw error;
     }
 };
 
-export default fetchUserData;
+export default fetchAdvancedUserData;
