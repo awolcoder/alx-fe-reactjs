daed56a4d9a5b98f3120c78354883ea831cecf11
awolcoder <abdulbasitawil42@gmail.com>
2024-12-20 22:19:50 +0300

    modified

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,77 +1,36 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SearchBar from './components/SearchBar';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
-import SearchBar from './components/SearchBar';
 import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList';
 
-
-
-function App() {
-  return (
-    <>
-      
-    </>
-  )
-}
-
-
-// Task 0 App.jsx updated
-
-
 const App = () => {
   return (
-    <div>
+    <Router>
+      <div style={{ padding: '20px' }}>
       <h1>Recipe Sharing Application</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
-  );
-};
-
-// Task 1 App.jsx updated
-// src/App.jsx
-
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<RecipeList />} />
-        <Route path="/favorites" element={<FavoritesList />} />
-        <Route path="/recommendations" element={<RecommendationsLis />} />
-        <Route path="/add-recipe" element={<AddRecipeForm />} />
-        <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
-
-// Task 2 App.jsx updated
-
-import RecipeList from './components/RecipeList';
-
-const App = () => {
-  return (
-    <div className="app">
-      <h1>Recipe Sharing App</h1>
+        <nav>
+          <ul style={{ display: 'flex', gap: '10px', listStyle: 'none', padding: 0 }}>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/favorites">Favorites</Link></li>
+            <li><Link to="/recommendations">Recommendations</Link></li>
+            <li><Link to="/add-recipe">Add Recipe</Link></li>
+          </ul>
+        </nav>
       <SearchBar />
-      <RecipeList />
-    </div>
+      <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/favorites" element={<FavoritesList />} />
+          <Route path="/recommendations" element={<RecommendationsList />} />
+          <Route path="/add-recipe" element={<AddRecipeForm />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
-// Task 3 App.jsx updated 
-
-const App = () => {
-  return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <FavoritesList />
-      <RecommendationsList />
-    </div>
-  );
-};
-
-export default App
+export default App;
