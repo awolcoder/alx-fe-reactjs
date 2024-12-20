1797fcf33778442efbad24fdeb840f6073c4758b
awolcoder <abdulbasitawil42@gmail.com>
2024-12-20 17:41:15 +0300

    formik modified

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
+import RegistrationForm from './components/RegistrationForm';
+import FormikForm from './components/FormikForm';
 import './App.css'
 
 function App() {
@@ -8,53 +8,13 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-import React from 'react';
-import RegistrationForm from './components/RegistrationForm';
-
-const App = () => {
-  return (
-    <div>
-      <RegistrationForm />
-    </div>
-  );
-};
-
-import React from 'react';
-import RegistrationForm from './components/RegistrationForm';
-import FormikForm from './components/FormikForm';
-
-const App = () => {
-  return (
     <div>
       <RegistrationForm />
       <hr />
       <FormikForm />
     </div>
-  );
-};
+    </>
+  )
+}
 
 export default App
